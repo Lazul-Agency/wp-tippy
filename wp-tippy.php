4a54586ea178071ef4b4a5bb0e38f801cd18d003
GSIMCOX524 <GSIMCOX524@users.noreply.github.com>
2020-10-16 19:43:29 -0400

    Add WP Role Filter feature after revision

--- wp-tippy.php
+++ wp-tippy.php
@@ -28,7 +28,20 @@ wp_enqueue_script('popper', 'https://unpkg.com/@popperjs/core@2');
 */
 
 function createTippyInstance($elementID, $properties) {
-  echo "<script>
+    
+    //  If WP Role Filter exists, then validate the filter
+    if (isset($wpRoleFilter)) {
+        $wpCurrentUser = wp_get_current_user();
+        $wpCurrentRole = $wpCurrentUser -> roles[0];
+        if ($wpCurrentRole == $wpRoleFilter) {
+            echo "<script>
+      		tippy('$elementID', $properties);
+    		</script>";
+        }
+    }
+    
+    //  WP Role Filter unset, display for all users
+    echo "<script>
       		tippy('$elementID', $properties);
     		</script>";
 }
